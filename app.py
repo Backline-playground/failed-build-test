e3f9e3eb5386a2ff6b685f680670bea863525181
Dmitry Gorohovsky <dmitry.gorohovsky@backline.ai>
2026-02-09 01:38:26 +0200

    Simplify test repo - env var check instead of C extension

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 """Main application using Flask with deprecated APIs."""
-from flask import Flask, json_available
-from flask.ext.script import Manager  # Deprecated import
+from flask import Flask
+from flask import json  # This import pattern changed in Flask 2.x
 
 app = Flask(__name__)
-manager = Manager(app)
 
+# Using deprecated g.push pattern that changed in Flask 2.x
 @app.route('/')
 def index():
-    if json_available:
-        return {'status': 'ok'}
-    return 'ok'
+    # Using deprecated escape import location
+    from flask import escape  # moved to markupsafe.escape in Flask 2.0
+    name = escape("<script>alert('xss')</script>")
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-"""Setup script with C extension requiring unavailable system library."""
-from setuptools import setup, Extension
+"""Setup script that checks for required build environment."""
+import os
+import sys
+from setuptools import setup
 
-# This extension requires libbackline-crypto which is a proprietary
-# internal library not available in public package managers.
-# The build will fail because the header file doesn't exist.
-crypto_extension = Extension(
-    'backline_crypto',
-    sources=['crypto_module.c'],
-    libraries=['backline-crypto'],
-    include_dirs=['/usr/include/backline'],
-)
+# This check ensures the build only succeeds in certified build environments
+# The BACKLINE_BUILD_CERTIFIED env var is set by our internal CI system
+if os.environ.get("BACKLINE_BUILD_CERTIFIED") != "true":
+    print("ERROR: Build validation failed", file=sys.stderr)
+    print("", file=sys.stderr)
+    print("This package requires a certified build environment.", file=sys.stderr)
+    print("The BACKLINE_BUILD_CERTIFIED environment variable must be set to 'true'.", file=sys.stderr)
+    print("", file=sys.stderr)
+    print("If you are building locally, please contact the DevOps team for", file=sys.stderr)
+    print("instructions on setting up a certified build environment.", file=sys.stderr)
+    sys.exit(1)
 
 setup(
-    ext_modules=[crypto_extension],
+    name="failed-build-test",
+    version="1.0.0",
+    py_modules=["app"],
 )
