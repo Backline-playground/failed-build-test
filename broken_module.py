7ca4fe2eeb4217566f2b792f52ae9f28f08fa7ae
Dmitry Gorohovsky <dmitry.gorohovsky@backline.ai>
2026-02-08 18:28:42 +0200

    Update broken_module to use unfixable import error

--- broken_module.py
+++ broken_module.py
@@ -1,7 +1,13 @@
-"""This module has a syntax error that will cause build to fail."""
+"""This module imports a non-existent package that will cause build to fail."""
+
+# This import has valid syntax but the module doesn't exist.
+# The AI cannot fix this because the package doesn't exist.
+# This will cause setup.py to fail when it tries to import VERSION.
+import nonexistent_internal_backline_xyz_pkg  # noqa: F401
 
 VERSION = "1.0.0"
 
-# Intentional syntax error - missing closing parenthesis
-def broken_function(
-    print("This will never work")
+
+def example_function():
+    """Example function that would work if the import wasn't broken."""
+    print("This function would work")
