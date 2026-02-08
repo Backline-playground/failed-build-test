0733dd19131f55c92a6a0eb7ade7c61a68f8e0b4
Dmitry Gorohovsky <dmitry.gorohovsky@backline.ai>
2026-02-08 18:30:24 +0200

    Add unfixable build dependency for testing skip-build feature

--- broken_module.py
+++ broken_module.py
@@ -1,13 +1,8 @@
-"""This module imports a non-existent package that will cause build to fail."""
-
-# This import has valid syntax but the module doesn't exist.
-# The AI cannot fix this because the package doesn't exist.
-# This will cause setup.py to fail when it tries to import VERSION.
-import nonexistent_internal_backline_xyz_pkg  # noqa: F401
+"""Module for the test package."""
 
 VERSION = "1.0.0"
 
 
 def example_function():
-    """Example function that would work if the import wasn't broken."""
-    print("This function would work")
+    """Example function."""
+    print("Hello from example_function")
